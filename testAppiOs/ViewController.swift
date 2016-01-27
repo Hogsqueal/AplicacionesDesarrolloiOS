59c661b828f46e04bf10b9ae9354de8ada7d2f4e
Hogsqueal <Hogsqueal@macbook.home>
2016-01-26 22:38:57 -0500

    Initial Commit

---
+++ testAppiOs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testAppiOs
+//
+//  Created by Hogsqueal on 1/26/16.
+//  Copyright © 2016 Hogsqueal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
